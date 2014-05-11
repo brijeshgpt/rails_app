abb055ea574b50b6f17250a8098186dd33064e34
vinay <vinay@vinay.(none)>
2014-05-11 22:42:52 +0530

    fixed bug in user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
         format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @usererrors, status: :unprocessable_entity }
       end
     end
   end
@@ -42,11 +42,11 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'User was successfully updated his profile ' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessa }
       end
     end
   end
